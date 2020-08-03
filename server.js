08c3ba2822a944ca77b678d0907331c283a15958
Budi Gunawan <budiindra.g@gmail.com>
2020-08-03 11:45:38 +0700

    define routes

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ app.get("/",(req,res) => {
     res.send("API is running");
 })
 
+/// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
